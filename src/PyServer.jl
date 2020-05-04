60d46ab38a9ec857201bc35521547236bb992e11
Shuhei Kadowaki <aviatesk@gmail.com>
2020-05-04 19:40:25 +0900

    init

---
+++ src/PyServer.jl
@@ -0,0 +1,5 @@
+module PyCallServer
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PyCallServer
+using Test
+
+@testset "PyCallServer.jl" begin
+    # Write your own tests here.
+end
